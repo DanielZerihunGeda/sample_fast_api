d68658cd4babd30654a155f9bd60335480e1e48f
Daniel Zerihun Geda <152178755+DanielZerihunGeda@users.noreply.github.com>
2026-01-21 07:38:56 +0000

    Add CORS middleware and update response text

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from typing import Optional
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import aiohttp
 from bs4 import BeautifulSoup
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 async def fetch_text(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
@@ -21,13 +31,10 @@ async def root():
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-sammp = """EASE - Landmarks of TomorrowHomeAboutProjectsServicesContactHomeAboutProjectsServicesContactEngineering Solutions 
-for TomorrowSolving East Africa's most complex construction challenges through innovative problem-solving"""
-
 
 @app.get("/copilot/{query}")
 async def return_ans(query: str):
     text = await fetch_text("https://ease-int.com/")
-    return {"response": sammp}
+    return {"response": text}
 
 
